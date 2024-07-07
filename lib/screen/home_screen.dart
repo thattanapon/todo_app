2ab3668ca01aa9c5b1651ec2d3e88a9a84c8232c
Thattanapon Bunsombat <64143133@g.cmru.ac.th>
2024-07-07 16:34:54 +0700

    dsf

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:html';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,7 +11,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final String API_URL = 'http://127.0.0.1:5001/todos';
   List todoList = [];
+  Future<List> fetchTodoList() async {
+    final response = await http.get(Uri.parse(API_URL));
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load');
+    }
+  }
+
+  @override
+  initState() {
+    fetchTodoList();
+    super.initState();
+    fetchTodoList().then((value) {
+      setState(() {
+        todoList = value;
+      });
+    });
+  }
+  Future<void> createTodo() async {
+    final response = await http.post(
+      Url.parse(API_URL),
+      body: {'title': task.text},
+    ),
+  }.then((value)
+  )
+  }
 
   final task = TextEditingController();
 
@@ -64,7 +95,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: ListView.builder(
                 itemBuilder: (context, index) {
                   return ListTile(
-                    title: Text(todoList[index]),
+                    title: Text(todoList[index]['title']),
                     trailing: IconButton(
                       icon: Icon(Icons.delete),
                       onPressed: () => deleteTodo(index),
